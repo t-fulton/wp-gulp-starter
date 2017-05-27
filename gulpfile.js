7f077ab8ed0c7f4f4256b7269247d4aea5a1bb5f
t-fulton <travis.fulton@pollinate.com>
2017-05-26 17:09:15 -0700

    Add clean task, sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,12 @@
 var gulp = require('gulp'),
 	sass = require('gulp-sass'),
 	browserSync = require('browser-sync').create(),
-	useref = require('gulp-useref'),
 	concat = require('gulp-concat'),
 	uglify = require('gulp-uglify'),
 	gulpIf = require('gulp-if'),
+	autoprefix = require('gulp-autoprefixer'),
 	cssnano = require('gulp-cssnano'),
+	sourcemaps = require('gulp-sourcemaps'),
 	imagemin = require('gulp-imagemin'),
 	cache = require('gulp-cache'),
 	del = require('del'),
@@ -13,11 +14,15 @@ var gulp = require('gulp'),
 
 // SASS
 gulp.task('sass', function() {
-  return gulp.src('lib/scss/style.scss') 
-    .pipe(sass())
-    .pipe(gulp.dest('./'))
-    pipe(browserSync.reload({
-    	stream:true
+  	return gulp.src('lib/scss/style.scss') 
+  		.pipe(sourcemaps.init())
+		    .pipe(sass())
+		    .pipe(autoprefix())
+		    .pipe(cssnano())
+		.pipe(sourcemaps.write())
+	    .pipe(gulp.dest('./'))
+	    .pipe(browserSync.reload({
+	    	stream:true
     }))
 })
 
@@ -27,9 +32,29 @@ gulp.task('scripts', function() {
 			'./lib/js/vendor/jquery-3.2.1.min.js', 
 			'./lib/js/custom/*.js'
 		])
-		.pipe(concat('app.js'))
+		.pipe(sourcemaps.init())
+			// Enqueued in functions.php 
+			.pipe(concat('app.js'))
+			// Minify JS
+			.pipe(uglify())
+		.pipe(sourcemaps.write())
 		.pipe(gulp.dest('./lib/js'));
 })
+
+// Minify Images
+gulp.task('images', function() {
+	return gulp.src('lib/images/**/*.+(png|jpg|gif|svg)')
+		// Caching images that ran through imagemin
+		.pipe(cache(imagemin({
+			interlaced: true
+		})))
+		.pipe(gulp.dest('./dist/images'))
+});
+
+// Delete dist folder
+gulp.task('clean:dist', function() {
+	return del.sync('dist');
+});
 
 // BrowserSync
 gulp.task('browserSync', function() {
@@ -38,7 +63,15 @@ gulp.task('browserSync', function() {
 	})
 })
 
-// Watch
+// Build
+gulp.task('build', function(callback) {
+	runSequence('clean:dist',
+		['sass', 'scripts', 'images'],
+		callback
+	)
+});
+
+// Watch TODO: app.js is looping, need to watch for js changes but not watch the file that ultimately gets created
 gulp.task('watch', ['browserSync', 'sass', 'scripts'], function(){
 	gulp.watch('lib/scss/**/*.scss', ['sass']).on('change', browserSync.reload);
 	gulp.watch('template-parts/**/*.php', browserSync.reload);
